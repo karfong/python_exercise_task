bb4077fe8a43e9fde7fad401dad1f0cddd9a4a20
fong <chankarfong8888@Gmail.com>
2024-02-14 09:43:04 +0800

    exercise 2 code updated finish

--- exercise_2.py
+++ exercise_2.py
@@ -13,3 +13,23 @@ result = savings * factor ** 7
 
 # Print out result
 print(result)
+
+a = 5
+b = 2.5
+c = 'Hello Python!'
+
+# Print types of a, b, and c
+# Example: print(type(a))
+
+# Several variables to experiment with
+savings = 100
+factor = 1.1
+desc = "compound interest"
+
+# Assign product of factor and savings to year1
+year1 = savings * factor
+# Print the type of year1
+print(type(year1))
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+print(doubledesc)
