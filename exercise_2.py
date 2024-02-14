5e07a46edf6cf7537a4ab02fd2d02e504c1b888d
fong <chankarfong8888@Gmail.com>
2024-02-14 09:08:12 +0800

    Add code to calculate result

---
+++ exercise_2.py
@@ -0,0 +1,15 @@
+# Create a variable savings
+x = 5
+# Print out savings
+print(x)
+
+# Create a variable savings and assign value 100
+savings = 100
+# Create a variable factor
+factor = 1.10
+
+# Calculate result
+result = savings * factor ** 7
+
+# Print out result
+print(result)
