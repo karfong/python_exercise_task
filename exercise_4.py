c093cbea93d477e635e12d70050ddf10ebe626d4
fong <chankarfong8888@Gmail.com>
2024-02-15 09:26:12 +0800

    Add exercise 4 file

---
+++ exercise_4.py
@@ -0,0 +1,65 @@
+#Exercise 1
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[-1]) 
+
+# Print out the area of the living room
+print(areas[5])   
+
+#Exercise 2
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3] + areas[7] 
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
+
+#Exercise 3
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Use slicing to create downstairs
+downstairs = areas[:6]
+
+# Use slicing to create upstairs
+upstairs = areas[6:]
+
+# Print out downstairs and upstairs
+print(downstairs)
+print(upstairs)
+
+
+#Exercise 4
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Alternative slicing to create downstairs
+downstairs = areas[:-4]
+
+# Alternative slicing to create upstairs
+upstairs = areas[-4:]
+
+#Exercise 5
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath]]
+
+# Print type of first element of last elemnt (list) of house
+print(type(house[-1][0]))
