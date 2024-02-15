d8ad34bd16bdec979d7ddd0310721036b19273c2
fong <chankarfong8888@Gmail.com>
2024-02-15 11:48:44 +0800

    Add exercise 6 code

---
+++ exercise_6.py
@@ -0,0 +1,35 @@
+#Exercise 1 
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out type of var1
+print(type(var1))
+
+# Print out length of var1
+print(len(var1))
+
+# Convert var2 to an integer: out2
+out2 = int(var2)
+print(out2)
+
+#Exercise 2
+# open up the documentation of complex() function using help or ?
+# help(complex)
+
+#Exercise 3
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full, reverse=True)
+
+# Print out full_sorted
+print(full_sorted)
+
+
+
